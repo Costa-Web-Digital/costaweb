a256d4737de37b4d4e2601dec1bd3403c2141024
Caio Costa <caiocrcosta@gmail.com>
2026-03-29 18:05:10 -0300

    refactor: change title to gray-800

--- src/components/Hero/Title.jsx
+++ src/components/Hero/Title.jsx
@@ -1,5 +1,5 @@
 export const Title = () => (
-	<h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-6 leading-none">
+	<h1 className="text-gray-800 text-4xl sm:text-5xl lg:text-6xl font-bold mb-6 leading-none">
 		Seu negócio
 		<div className="bg-gradient-to-br from-secondary to-tertiary text-transparent bg-clip-text">na internet</div>
 		do jeito certo
