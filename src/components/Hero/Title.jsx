fb61f7858f99d1c1fb5efc0391c043d244ffab09
Caio Costa <caiocrcosta@gmail.com>
2026-03-30 22:53:13 -0300

    fix: add line breaks

--- src/components/Hero/Title.jsx
+++ src/components/Hero/Title.jsx
@@ -1,7 +1,7 @@
 export const Title = () => (
 	<h1 className="text-ink text-4xl sm:text-5xl lg:text-6xl font-semibold mb-6 leading-none">
-		Seu negócio
-		<span className="bg-gradient-to-br from-secondary to-tertiary text-transparent bg-clip-text"> na internet </span>
+		Seu negócio <br />
+		<span className="bg-gradient-to-br from-secondary to-tertiary text-transparent bg-clip-text"> na internet </span><br />
 		do jeito certo
 	</h1>
 );
